b774ec35fae9876af85c2e2b8a2fdd1a95d19807
surfaceyu <surfaceyu@outlook.com>
2022-08-09 17:12:43 +0800

    1.去除不需要的导入组件

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,7 +4,6 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from "./router"
 
-import Functions from './components/Functions.vue';
 import AboutGame from './components/AboutGame.vue'
 import Map from './components/Map.vue'
 
@@ -12,7 +11,6 @@ const app = createApp(App)
 app.use(ElementPlus)
 app.use(router)
 
-app.component("Functions", Functions)
 app.component("AboutGame", AboutGame)
 app.component("Map", Map)
 
